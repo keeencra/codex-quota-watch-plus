b61e8a6cbc496265325bb0a040cf3d6c579a19cb
Zane <keeencra@gmail.com>
2026-09-13 20:23:31 +0800

    release: v3.1.0 shared-widgets

---
+++ shared-widgets/WidgetAppearance.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+extension Color {
+    static let codexWidgetCredit = Color(red: 0.66, green: 0.59, blue: 1)
+    static let codexWidgetGreen = Color(red: 0.19, green: 0.82, blue: 0.35)
+    static let codexWidgetBlue = Color(red: 0.23, green: 0.62, blue: 1.0)
+    static let codexWidgetAmber = Color(red: 1.0, green: 0.70, blue: 0.04)
+    static let codexWidgetRed = Color(red: 1.0, green: 0.27, blue: 0.23)
+    static let codexWidgetSecondary = Color(red: 0.92, green: 0.92, blue: 0.96).opacity(0.58)
+    static let codexWidgetGreenTrack = Color(red: 0.09, green: 0.14, blue: 0.10)
+    static let codexWidgetBlueTrack = Color(red: 0.09, green: 0.14, blue: 0.23)
+}
